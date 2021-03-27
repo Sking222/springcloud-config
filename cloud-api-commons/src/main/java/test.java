8c2d20f80a8b2af47214303431f0b2ca19a1b80d
Sking222 <1292396848@qq.com>
2021-03-27 11:01:13 +0800

    spring-cloud

---
+++ cloud-api-commons/src/main/java/test.java
@@ -0,0 +1,37 @@
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+
+import java.math.BigDecimal;
+
+public class test {
+    public static void main(String[] args) {
+//        DateTime date = DateUtil.date();
+//        int i = date.monthStartFromOne();
+//        System.out.println(DateUtil.formatDate(date));
+//        System.out.println(i);
+//
+//      DateTime dateTime = DateUtil.lastMonth();
+//        int month = DateUtil.month(dateTime);
+//       System.out.println(month+1);
+//
+//        int i1 = DateUtil.thisYear();
+//        System.out.println(i1-1);
+
+//        int i = 10;
+//        String a ="10";
+//        System.out.println(a.equals(i));
+
+//         double i=700;
+//         double i2=600;
+//
+//         double d= i / i2;
+//        BigDecimal B =new BigDecimal(i / i2);
+//        System.out.println(d);
+//        System.out.println(B);
+
+        String s1="1234";
+        String s2="1234";
+        System.out.println(s1.equals(s2));
+
+    }
+}
